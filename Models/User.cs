7c1565081e571a9157b31e290c7a5bab6a80989b
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-03-28 21:30:59 +0200

    chore: make email in User unique

--- Models/User.cs
+++ Models/User.cs
@@ -1,9 +1,12 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace RitmSync_backend.Models;
 
 [Table("users")]
+[Index(nameof(Email), IsUnique = true)]
 public class User
 {
     [Column("id")]
