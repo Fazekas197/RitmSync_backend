c4fedc9ccc31cd8678bd758e3a6707adb22d74ed
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-03-28 20:30:28 +0200

    chore: add User Genres Model

---
+++ Models/UserGenres.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RitmSync_backend.Models;
+
+[Table("user_genres")]
+[PrimaryKey(nameof(UserId), nameof(GenresId))]
+public class UserGenres
+{
+    [Column("user_id")]
+    [Required]
+    public int UserId { get; set; }
+
+    [Column("genres_id")]
+    [Required]
+    public int GenresId { get; set; }
+
+    [ForeignKey("UserId")]
+    public User User { get; set; }
+
+    [ForeignKey("GenresId")]
+    public Genres Genre { get; set; }
+}
