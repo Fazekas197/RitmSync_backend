1c52897b46d2dc7a29cdb6f930195d88f056cec3
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-03-28 20:26:12 +0200

    chore: add User Experience Model

---
+++ Models/UserExperience.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RitmSync_backend.Models;
+
+[Table("user_experience")]
+public class UserExperience
+{
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("user_id")]
+    [Required]
+    public int UserId { get; set; }
+
+    [Column("project_name")]
+    [Required]
+    [MaxLength(60)]
+    public string ProjectName { get; set; }
+
+    [Column("start_date")]
+    [Required]
+    public DateTime Start { get; set; }
+
+    [Column("end_date")]
+    public DateTime End { get; set; } = null
+
+    [ForeignKey("UserId")]
+    public User User { get; set; }
+}
