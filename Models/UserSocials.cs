b5a131a39bf857b84d394d8ee9f2702363eecc42
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-03-28 20:36:27 +0200

    chore add User Socials Model

---
+++ Models/UserSocials.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RitmSync_backend.Models;
+
+[Table("user_socials")]
+public class UserSocials
+{
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("user_id")]
+    [Required]
+    public int UserId { get; set; }
+
+    [Column("platform")]
+    [Required]
+    [MaxLength(60)]
+    public string Platform { get; set; }
+
+    [Column("link")]
+    [Required]
+    public string Link { get; set; }
+
+    [ForeignKey("UserId")]
+    public User User { get; set; }
+}
