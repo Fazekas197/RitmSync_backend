a63ddeb4f26f420ac277d27751e5bd692edcfcc4
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-03-28 20:11:01 +0200

    chore: add County model

---
+++ Models/County.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RitmSync_backend.Models;
+
+[Table("county")]
+public class County
+{
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("name")]
+    [Required]
+    [MaxLength(20)]
+    public string Name { get; set; }
+
+    [Column("abv")]
+    [Required]
+    [MaxLength(2)]
+    public string Abv { get; set; }
+}
