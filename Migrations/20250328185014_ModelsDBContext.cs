3022e2085fb20fd713266be2c2ab5604dcd58127
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-03-28 20:52:37 +0200

    chore: migrate db

---
+++ Migrations/20250328185014_ModelsDBContext.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace RitmSync_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModelsDBContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "genres",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_genres", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "instruments",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_instruments", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_experience",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    user_id = table.Column<int>(type: "integer", nullable: false),
+                    project_name = table.Column<string>(type: "character varying(60)", maxLength: 60, nullable: false),
+                    start_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    end_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user_experience", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_user_experience_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_socials",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    user_id = table.Column<int>(type: "integer", nullable: false),
+                    platform = table.Column<string>(type: "character varying(60)", maxLength: 60, nullable: false),
+                    link = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user_socials", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_user_socials_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_genres",
+                columns: table => new
+                {
+                    user_id = table.Column<int>(type: "integer", nullable: false),
+                    genres_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user_genres", x => new { x.user_id, x.genres_id });
+                    table.ForeignKey(
+                        name: "FK_user_genres_genres_genres_id",
+                        column: x => x.genres_id,
+                        principalTable: "genres",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_user_genres_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_instruments",
+                columns: table => new
+                {
+                    user_id = table.Column<int>(type: "integer", nullable: false),
+                    instrument_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user_instruments", x => new { x.user_id, x.instrument_id });
+                    table.ForeignKey(
+                        name: "FK_user_instruments_instruments_instrument_id",
+                        column: x => x.instrument_id,
+                        principalTable: "instruments",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_user_instruments_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_experience_user_id",
+                table: "user_experience",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_genres_genres_id",
+                table: "user_genres",
+                column: "genres_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_instruments_instrument_id",
+                table: "user_instruments",
+                column: "instrument_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_socials_user_id",
+                table: "user_socials",
+                column: "user_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "user_experience");
+
+            migrationBuilder.DropTable(
+                name: "user_genres");
+
+            migrationBuilder.DropTable(
+                name: "user_instruments");
+
+            migrationBuilder.DropTable(
+                name: "user_socials");
+
+            migrationBuilder.DropTable(
+                name: "genres");
+
+            migrationBuilder.DropTable(
+                name: "instruments");
+        }
+    }
+}
