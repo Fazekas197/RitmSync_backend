28aa185ad623b0bc561ed87322228a13f2d5a061
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-03-28 20:49:55 +0200

    chore: add models to db context

--- Data/AppDBContext.cs
+++ Data/AppDBContext.cs
@@ -6,5 +6,14 @@ namespace RitmSync_backend.Data;
 public class AppDBContext : DbContext
 {
     public DbSet<User> Users { get; set; } = null!;
+    public DbSet<County> County { get; set; } = null!;
+    public DbSet<Genres> Genres { get; set; } = null!;
+    public DbSet<Instruments> Instruments { get; set; } = null!;
+    public DbSet<UserExperience> UserExperience { get; set; } = null!;
+    public DbSet<UserGenres> UserGenres { get; set; } = null!;
+    public DbSet<UserInstruments> UserInstruments { get; set; } = null!;
+    public DbSet<UserSocials> UserSocials { get; set; } = null!;
+
+
     public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
 }
---
+++ Migrations/20250328184512_UserTabels.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace RitmSync_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserTabels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "county_id",
+                table: "users",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "description",
+                table: "users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "email",
+                table: "users",
+                type: "character varying(40)",
+                maxLength: 40,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "open_for_collab",
+                table: "users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "pass_hash",
+                table: "users",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "phone",
+                table: "users",
+                type: "character varying(10)",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "county",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    abv = table.Column<string>(type: "character varying(2)", maxLength: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_county", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_county_id",
+                table: "users",
+                column: "county_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_users_county_county_id",
+                table: "users",
+                column: "county_id",
+                principalTable: "county",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_users_county_county_id",
+                table: "users");
+
+            migrationBuilder.DropTable(
+                name: "county");
+
+            migrationBuilder.DropIndex(
+                name: "IX_users_county_id",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "county_id",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "description",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "email",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "open_for_collab",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "pass_hash",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "phone",
+                table: "users");
+        }
+    }
+}
