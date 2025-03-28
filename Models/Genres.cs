28d23c2a42babc39c03e15356af7dae3792abb73
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-03-28 20:20:54 +0200

    chore: add Genres Model

---
+++ Models/Genres.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RitmSync_backend.Models;
+
+[Table("genres")]
+public class Genres
+{
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("name")]
+    [Required]
+    [MaxLength(30)]
+    public string Name { get; set; }
+}
