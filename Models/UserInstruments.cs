94491c8128a271d8d29e2a6dfb4ae5aee049305a
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-03-28 20:29:09 +0200

    chore: add User Instruments  Model

---
+++ Models/UserInstruments.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RitmSync_backend.Models;
+
+[Table("user_instruments")]
+[PrimaryKey(nameof(UserId), nameof(InstrumentId))]
+public class UserInstruments
+{
+    [Column("user_id")]
+    [Required]
+    public int UserId { get; set; }
+
+    [Column("instrument_id")]
+    [Required]
+    public int InstrumentId { get; set; }
+
+    [ForeignKey("UserId")]
+    public User User { get; set; }
+
+    [ForeignKey("InstrumentId")]
+    public Instruments Instrument { get; set; }
+}
