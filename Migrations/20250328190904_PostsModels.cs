e28484c19a1b63f05927140bba08ddd1a224dbd1
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-03-28 21:09:48 +0200

    chore: add db migration
    Add migration for Posts related Models

---
+++ Migrations/20250328190904_PostsModels.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace RitmSync_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class PostsModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "posts",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    user_id = table.Column<int>(type: "integer", nullable: false),
+                    title = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: false),
+                    county_id = table.Column<int>(type: "integer", nullable: false),
+                    description = table.Column<string>(type: "text", nullable: false),
+                    email = table.Column<string>(type: "text", nullable: true),
+                    phone = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_posts", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_posts_county_county_id",
+                        column: x => x.county_id,
+                        principalTable: "county",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_posts_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "post_genres",
+                columns: table => new
+                {
+                    post_id = table.Column<int>(type: "integer", nullable: false),
+                    genres_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_post_genres", x => new { x.post_id, x.genres_id });
+                    table.ForeignKey(
+                        name: "FK_post_genres_genres_genres_id",
+                        column: x => x.genres_id,
+                        principalTable: "genres",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_post_genres_posts_post_id",
+                        column: x => x.post_id,
+                        principalTable: "posts",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "post_instruments",
+                columns: table => new
+                {
+                    post_id = table.Column<int>(type: "integer", nullable: false),
+                    instrument_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_post_instruments", x => new { x.post_id, x.instrument_id });
+                    table.ForeignKey(
+                        name: "FK_post_instruments_instruments_instrument_id",
+                        column: x => x.instrument_id,
+                        principalTable: "instruments",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_post_instruments_posts_post_id",
+                        column: x => x.post_id,
+                        principalTable: "posts",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "post_socials",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    post_id = table.Column<int>(type: "integer", nullable: false),
+                    platform = table.Column<string>(type: "character varying(60)", maxLength: 60, nullable: false),
+                    link = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_post_socials", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_post_socials_posts_post_id",
+                        column: x => x.post_id,
+                        principalTable: "posts",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_post_genres_genres_id",
+                table: "post_genres",
+                column: "genres_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_post_instruments_instrument_id",
+                table: "post_instruments",
+                column: "instrument_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_post_socials_post_id",
+                table: "post_socials",
+                column: "post_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_posts_county_id",
+                table: "posts",
+                column: "county_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_posts_user_id",
+                table: "posts",
+                column: "user_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "post_genres");
+
+            migrationBuilder.DropTable(
+                name: "post_instruments");
+
+            migrationBuilder.DropTable(
+                name: "post_socials");
+
+            migrationBuilder.DropTable(
+                name: "posts");
+        }
+    }
+}
