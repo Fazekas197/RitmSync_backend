aa9e543c8e69d68deba987b9ce54b1c318ac7ab5
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-04-24 15:44:57 +0300

    chore: add constructor to static data DTO

--- DTO/StaticDataDTO.cs
+++ DTO/StaticDataDTO.cs
@@ -7,4 +7,11 @@ public class StaticDataDTO
     public List<County> Counties { get; set; } = new List<County>();
     public List<Genres> Genres { get; set; } = new List<Genres>();
     public List<Instruments> Instruments { get; set; } = new List<Instruments>();
+
+    public StaticDataDTO(List<County> counties, List<Genres> genres, List<Instruments> instruments)
+    {
+        Counties = counties;
+        Genres = genres;
+        Instruments = instruments;
+    }
 }
