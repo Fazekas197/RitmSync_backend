5b6f62797aa33637fd2818de161b1ae2301bf224
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-04-24 15:37:59 +0300

    chore: create static data DTO

---
+++ DTO/StaticDataDTO.cs
@@ -0,0 +1,10 @@
+using RitmSync_backend.Models;
+
+namespace RitmSync_backend.DTO;
+
+public class StaticDataDTO
+{
+    public List<County> Counties { get; set; } = new List<County>();
+    public List<Genres> Genres { get; set; } = new List<Genres>();
+    public List<Instruments> Instruments { get; set; } = new List<Instruments>();
+}
