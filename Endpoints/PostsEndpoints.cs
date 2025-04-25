b89c365d56ef4f11f11f645cec1cc4eb2f71d277
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-04-25 13:13:08 +0300

    feat: add ednpoint to get post by id

--- Endpoints/PostsEndpoints.cs
+++ Endpoints/PostsEndpoints.cs
@@ -45,5 +45,33 @@ public static class PostsEndpoint
                 return Results.Problem(e.Message);
             }
         });
+
+        group.MapGet("/{id}", async (AppDBContext db, int id) =>
+        {
+            try
+            {
+                var postFromDB = await db.Posts.FindAsync(id);
+
+                var instruments = await db.PostsInstruments
+                    .Where(pi => pi.PostId == postFromDB!.Id)
+                    .Select(pi => pi.Instrument!.Name!).ToListAsync();
+
+                var genres = await db.PostsGenres
+                    .Where(pg => pg.PostId == postFromDB!.Id)
+                    .Select(pg => pg.Genre!.Name!).ToListAsync();
+
+                var socials = await db.PostsSocials
+                    .Where(ps => ps.PostId == postFromDB!.Id)
+                    .Select(ps => ps.Link!).ToListAsync();
+
+                PostDTO postDTO = new PostDTO(postFromDB!, instruments, genres, socials);
+                return Results.Ok(postDTO);
+            }
+            catch (System.Exception e)
+            {
+
+                return Results.Problem(e.Message); ;
+            }
+        });
     }
 }
