d0f47fcc103193dd2d3b7e4f1ef4358082321f3b
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-04-25 12:29:20 +0300

    chore: add short desc to Post DTO

--- DTO/PostDTO.cs
+++ DTO/PostDTO.cs
@@ -10,6 +10,7 @@ public class PostDTO
     public string? Title { get; set; }
     public string? County { get; set; }
     public string? Desc { get; set; }
+    public string? ShortDesc { get; set; }
     public string? Email { get; set; }
     public string? Phone { get; set; }
     public List<Instruments> Instruments { get; set; } = new List<Instruments>();
@@ -23,6 +24,7 @@ public class PostDTO
         Title = post.Title;
         County = post.County?.Name;
         Desc = post.Desc;
+        ShortDesc = Desc?.Length < 100 ? Desc : Desc?.Substring(0, 100) + "...";
         Email = post.Email;
         Phone = post.Phone;
         Instruments = instruments;
