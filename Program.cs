4028672d9f2a15675c96e3829d458717ff6a3cae
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-04-25 15:02:09 +0300

    chore: add cors

--- Program.cs
+++ Program.cs
@@ -13,7 +13,20 @@ var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
 builder.Services.AddDbContext<AppDBContext>(options =>
     options.UseNpgsql(connectionString));
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors("AllowAll");
 
 using (var scope = app.Services.CreateScope())
 {
