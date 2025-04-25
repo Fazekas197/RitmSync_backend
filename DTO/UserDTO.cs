bc7d760037dd4eade0f569c29b37c64b6c942ba2
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-04-25 19:03:19 +0300

    chore: add UserDTO

---
+++ DTO/UserDTO.cs
@@ -0,0 +1,36 @@
+using RitmSync_backend.Models;
+
+
+namespace RitmSync_backend.DTO;
+
+public class UserDTO
+{
+    public int Id { get; set; }
+    public string? Email { get; set; }
+    public string? FullName { get; set; }
+    public bool OpenForCollab { get; set; } = true;
+    public string? Phone { get; set; }
+    public string? Desc { get; set; }
+    public string? ShortDesc { get; set; }
+    public string? County { get; set; }
+    List<UserExperience> Experience { get; set; } = new List<UserExperience>();
+    List<string> Genres { get; set; } = new List<string>();
+    List<string> Instruments { get; set; } = new List<string>();
+    List<SocialDTO> Socials { get; set; } = new List<SocialDTO>();
+
+    public UserDTO(User user, List<UserExperience> experience, List<string> genres, List<string> instruments, List<SocialDTO> socials)
+    {
+        Id = user.Id;
+        Email = user.Email;
+        FullName = user.LastName + " " + user.FirstName;
+        OpenForCollab = user.OpenForCollab;
+        Phone = user.Phone;
+        Desc = user.Desc;
+        ShortDesc = Desc?.Length < 200 ? Desc : Desc?.Substring(0, 200) + "...";
+        County = user.County?.Name;
+        Experience = experience;
+        Genres = genres;
+        Instruments = instruments;
+        Socials = socials;
+    }
+}
