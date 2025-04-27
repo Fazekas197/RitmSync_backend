d22697a3fb04cbe7d400e26c318e821d30286847
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-04-27 23:54:55 +0300

    chore: remove unused endpoints

--- Program.cs
+++ Program.cs
@@ -35,9 +35,6 @@ using (var scope = app.Services.CreateScope())
 }
 
 app.MapUserEndpoints();
-app.MapCountyEndpoints();
-app.MapInstrumentsEndpoints();
-app.MapGenresEndpoints();
 app.MapStaticDataEndpoints();
 app.MapPostsEndpoints();
 
