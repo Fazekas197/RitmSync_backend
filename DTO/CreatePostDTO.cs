a8db15cc1812510921ccf67bd56c9192ee8989e7
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-05-06 16:54:44 +0300

    chore: add CreatePost DTO

---
+++ DTO/CreatePostDTO.cs
@@ -0,0 +1,16 @@
+using RitmSync_backend.Models;
+
+namespace RitmSync_backend.DTO;
+
+public class CreatePostDTO
+{
+    public int? UserId { get; set; }
+    public string? Title { get; set; }
+    public int? CountyId { get; set; }
+    public string? Desc { get; set; }
+    public string? Email { get; set; }
+    public string? Phone { get; set; }
+    public List<int> Instruments { get; set; } = new List<int>();
+    public List<int> Genres { get; set; } = new List<int>();
+    public List<SocialDTO> Socials { get; set; } = new List<SocialDTO>();
+}
